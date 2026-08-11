eb0ec80dc003569e8ff893da6b09fc7e1998508e
eesha12345 <eesha24mansoor@gmail.com>
2026-08-11 15:12:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import joblib
 
 # Load model and vectorizer
-model = joblib.load("src/models/sentiment_model.pkl")
-vectorizer = joblib.load("src/models/vectorizer.pkl")
+model = joblib.load("sentiment_model.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
 
 # Streamlit page
 st.set_page_config(
